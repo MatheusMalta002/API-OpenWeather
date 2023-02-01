46a2ba788239e831ac86b88216d12663e3659e7b
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-02-01 12:43:47 -0300

    Adição da API unsplash e codigo JS

--- Climatescript.js
+++ Climatescript.js
@@ -8,13 +8,18 @@ const temperatura_max = document.getElementById("temp_max")
 const umidade = document.getElementById("umidade")
 const nuvens = document.getElementById("nuvens")
 const vento = document.getElementById("vento")
+const bandeiraPais = document.getElementById("bandeira-pais")
+const Body = document.getElementById("img-cidades")
+
 
 const iconCeu = document.getElementById("céu")
 const Ceu = document.getElementById("ceu")
 
 const chaveAPI = "3851ff8a5f56eb5aa79d1e70c5e70887"
+const keyAPIunsplash = "-O_8nwSvvHCmjm5zXIfaiRG1w0CoIFsR9CiD3X473bo"
 
-//captura os dados da API
+
+//captura os dados da API openwheater
 async function getDadosAPI(cidade){
 
     const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${chaveAPI}&lang=pt_br`
@@ -40,18 +45,61 @@ async function imprimeDadosAPI(cidade){
         temperatura_max.innerText = `${Math.ceil(dados.main.temp_max)}°`
         umidade.innerText = `${dados.main.humidity}%`
         nuvens.innerText = dados.weather[0].description
-        vento.innerText = `${Math.round(dados.wind.speed*1.60934)} Km/h`
+        vento.innerText = `${Math.round(dados.wind.speed*3.6)} Km/h`
 
         iconCeu.setAttribute("src", `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`)
         Ceu.setAttribute("src", `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`)
-        
-        console.log(dados)
+
+        //API das bandeiras dos países
+        bandeiraPais.setAttribute("src", `https://flagsapi.com/${dados.sys.country}/flat/64.png`)
     }
+     
+}
+
+// captura a url da API
+async function getUnsplashApi(cidade){
+ 
+    const apiURL = `https://api.unsplash.com/search/photos?query=${cidade}&client_id=${keyAPIunsplash}&per_page=5`
+    
+    const resposta = await fetch(apiURL)
+    
+    const dados = await resposta.json()
+
+    return dados
+}
+
+// Exibe a imagem na tela
+
+async function exibeUnsplashApi(cidade){
+
+    const dados = await getUnsplashApi(cidade)
+
+    imgAleatoria = Math.round(Math.random()*5)
+
+    Body.style.backgroundImage=`url(${dados.results[imgAleatoria].urls.raw})`
     
 }
+
+//  Eventos
 
 btnEnvia.addEventListener("click", ()=>{
 
     imprimeDadosAPI(entradaTexto.value)
+    exibeUnsplashApi(entradaTexto.value)
+    entradaTexto.value = ""
 })
+
+window.addEventListener("keypress", (evento)=>{
+
+    if(evento.key == "Enter"){
+        imprimeDadosAPI(entradaTexto.value)
+        exibeUnsplashApi(entradaTexto.value)
+        entradaTexto.value = ""
+    }      
+})
+
+// valor default para cidade
+
+imprimeDadosAPI("Brasília")
+exibeUnsplashApi("Brasília")
 
